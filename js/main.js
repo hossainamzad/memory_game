b8bc5a9660eb84faad567f7236dbe4106dd2d52d
amzad hossain <amzad.hossain@baruchmail.cuny.edu>
2017-07-11 17:26:56 -0400

    add logic to the game using if and else statement

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardFour;
-var cardThree;
-console.log("User flipped: " + cardOne);
-console.log("User flipped: " + cardTwo);
+var cards = ["queen", "queen","king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
+console.log("User flipped queen");
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2){
+  if (cardsInPlay[0] === cardsInPlay[1] ){
+    alert("You have found a match!");
+  }else{
+    alert("Sorry, Try again");
+  }
+}
