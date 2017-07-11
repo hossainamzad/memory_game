b7fc49ca35b1c8c53725bf6cd5c5f3b988c98aeb
amzad hossain <amzad.hossain@baruchmail.cuny.edu>
2017-07-11 19:29:41 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,17 +1,30 @@
 var cards = ["queen", "queen","king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[1];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped queen");
-console.log("User flipped king");
-
-if (cardsInPlay.length === 2){
-  if (cardsInPlay[0] === cardsInPlay[1] ){
-    alert("You have found a match!");
-  }else{
-    alert("Sorry, Try again");
+// this function is for chcking card matches.
+var checkForMatch = function(){
+  if (cardsInPlay.length === 2){
+    if (cardsInPlay[0] === cardsInPlay[1] ){
+      console.log("You have found a match!");
+    }else{
+      console.log("Sorry, Try again");
+    }
   }
 }
+var flipCard = function(cardId){
+  console.log("User flipped " + cards[cardId]);
+   cardsInPlay.push(cards[cardId]);
+
+   checkForMatch();
+
+  // var cardOne = cards[0];
+  // var cardTwo = cards[1];
+  // cardsInPlay.push(cardOne);
+  // cardsInPlay.push(cardTwo);
+
+  // console.log("User flipped queen");
+  // console.log("User flipped king");
+
+}
+flipCard(2);
+flipCard(1);
+
