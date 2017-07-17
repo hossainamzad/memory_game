2ba7fa6340a24d641b280ac19f6702270ebd9264
amzad hossain <amzad.hossain@baruchmail.cuny.edu>
2017-07-16 20:25:15 -0400

    add dom features and manipulate elements with dom

--- js/main.js
+++ js/main.js
@@ -27,25 +27,37 @@ var cardsInPlay = [];
 var checkForMatch = function(){
   // this stateent is checking whether the user has played two cards and the number is .
   if (cardsInPlay.length === 2){
-    if (cardsInPlay[0] === cardsInPlay[1] ){
+    if (cardsInPlay[0].rank === cardsInPlay[1].rank ){
+
+       alert("You found a match!");
+
       console.log("You have found a match!");
-    }else{
-      alert("Sorry, Try again");
+    }else if (cardsInPlay[2].rank === cardsInPlay[3].rank){
       //to console log, if yoou choose to do it over alert, turn the following line on:
       // console.log("Sorry, Try again");
+       alert("You found a match!");
+       console.log("You have a match too.");
+
+
+    }else {
+      alert("Sorry, Try again");
+      console.log("you clicked one king and queen.");
+
 
     }
   }
 }
-var flipCard = function(cardId){
+
+var flipCard = function(){
+  var cardId = this.getAttribute('data-id');
+  this.setAttribute('src',cards[cardId].cardImage);
   console.log("User flipped " + cards[cardId].rank);
-   cardsInPlay.push(cards[cardId].rank);
+   cardsInPlay.push(cards[cardId]);
    console.log("The card image is: " + cards[cardId].cardImage);
    console.log("The card suit is: " + cards[cardId].suit);
 
 
    checkForMatch();
-
   // var cardOne = cards[0];
   // var cardTwo = cards[1];
   // cardsInPlay.push(cardOne);
@@ -54,9 +66,20 @@ var flipCard = function(cardId){
   // console.log("User flipped queen");
   // console.log("User flipped king");
 
-}
-flipCard(2);
-flipCard(1);
+};
 
+// flipCard(2);
+// flipCard(1);
 
+var createBoard = function(){
+  for (var i = 0; i < cards.length; i++){
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute("src", "images/back.png");
+    cardElement.setAttribute("data-id", i);
+    cardElement.addEventListener("click", flipCard);
+    document.querySelector("#game-board").appendChild(cardElement);
+
+  }
+};
+createBoard();
 
--- js/practice.js
+++ js/practice.js
@@ -11,3 +11,5 @@ function fizzBuzz(){
 }
 
 fizzBuzz();
+
+
